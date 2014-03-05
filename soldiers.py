2b162403715a4b6a744c43199463d4b137403076
ChosunOne <koreanhalo@gmail.com>
2014-03-04 20:18:19 -0600

    Added DNA
    Now we can track the mutations happening.

--- soldiers.py
+++ soldiers.py
@@ -1,189 +1,235 @@
 import random
 import pdb
+from collections import OrderedDict
 
 #pdb.set_trace()
 
 random.seed()
 
 class soldier:
-	def __init__(self, name = None, baseHealth = None, health = None, damage = None, armor = None):
-		
-		if name == None:
-			self.name = '0'
-		else:
-			self.name = name
+    def __init__(self, name = None, baseHealth = None, health = None, damage = None, armor = None, dna = None):
 
-		if baseHealth == None:
-			self.baseHealth = 100
-		else:
-			self.baseHealth = baseHealth
+        self.id = random.randint(0, 100000000000)
+        self.wins = 0
 
-		if health == None:
-			self.health = baseHealth
-		else:
-			self.health = health
+        if dna == None:
+            self.dna = ''
+        else:
+            self.dna = dna 
 
-		if damage == None:
-			self.damage = 1
-		else:
-			self.damage = damage
+        if name == None:
+            self.name = '0'
+        else:
+            self.name = name
 
-		if armor == None:
-			self.armor = 0
-		else:
-			self.armor = armor
+        if baseHealth == None:
+            self.baseHealth = 100
+        else:
+            self.baseHealth = baseHealth
 
-	def attack(self, soldier):
-		if self.damage - soldier.armor <= 0:
-			pass
-		else:
-			soldier.health = soldier.health - (self.damage - soldier.armor)
-			
-			#DEBUG
-			#print(self.name, 'attacked', soldier.name, 'for', self.damage - soldier.armor, 'damage.')
-			#print(soldier.name, 'now has', soldier.health, 'health')
+        if health == None:
+            self.health = baseHealth
+        else:
+            self.health = health
+
+        if damage == None:
+            self.damage = 1
+        else:
+            self.damage = damage
+
+        if armor == None:
+            self.armor = 0
+        else:
+            self.armor = armor
+
+    def attack(self, soldier):
+        if self.damage - soldier.armor <= 0:
+            pass
+        else:
+            soldier.health = soldier.health - (self.damage - soldier.armor)
+
+            #DEBUG
+            #print(self.name, 'attacked', soldier.name, 'for', self.damage - soldier.armor, 'damage.')
+            #print(soldier.name, 'now has', soldier.health, 'health')
 
 def reproduce(sol):
-	mutate = random.randint(0, 100)
-	offspring = soldier(str(int(sol.name) + 1), sol.baseHealth, sol.damage, sol.armor)
+    MUTATE_THRESHOLD = 15
+    mutate = random.randint(0, 100)
+    offspring = soldier(str(int(sol.name) + random.randint(1, 10)), sol.baseHealth, sol.damage, sol.armor, dna = sol.dna)
 
-	if mutate <= 5:
-		offspring.name = str(int(offspring.name) + 100)
+    if mutate <= MUTATE_THRESHOLD:
+        offspring.name = str(int(offspring.name) + 100)
+        offspring.dna += 'N'
 
-	mutate = random.randint(0, 100)
-	
-	if mutate <= 5:
-		offspring.baseHealth = random.randint(offspring.baseHealth - 5, offspring.baseHealth + 5)
-		offspring.health = offspring.baseHealth
+    mutate = random.randint(0, 100)
 
-	mutate = random.randint(0, 100)
-	
-	if mutate <= 5:
-		offspring.damage = random.randint(offspring.damage - 1, offspring.damage + 1)
+    if mutate <= MUTATE_THRESHOLD:
+        old = offspring.baseHealth
+        offspring.baseHealth = random.randint(offspring.baseHealth - 5, offspring.baseHealth + 5)
+        offspring.health = offspring.baseHealth
+        if old < offspring.baseHealth:
+            offspring.dna += 'H'
+        else:
+            offspring.dna += 'h'
 
-	if offspring.damage <= 0:
-			offspring.damage = 1
-	
-	mutate = random.randint(0, 100)
-	
-	if mutate <= 5:
-		offspring.armor = random.randint(offspring.armor - 1, offspring.armor + 1)
+    mutate = random.randint(0, 100)
 
-	if offspring.armor < 0:
-		offspring.armor = 0
+    if mutate <= MUTATE_THRESHOLD:
+        old = offspring.damage
+        offspring.damage = random.randint(offspring.damage - 1, offspring.damage + 1)
+        if old < offspring.damage:
+            offspring.dna += 'D'
+        else:
+            offspring.dna += 'd'
 
-	return offspring
+    if offspring.damage <= 0:
+            offspring.damage = 1
+    
+    mutate = random.randint(0, 100)
+    
+    if mutate <= MUTATE_THRESHOLD:
+        old = offspring.armor
+        offspring.armor = random.randint(offspring.armor - 1, offspring.armor + 1)
+        if old < offspring.armor:
+            offspring.dna += 'A'
+        else:
+            offspring.dna += 'a'
+
+    if offspring.armor < 0:
+        offspring.armor = 0
+
+    return offspring
 
 def fight(sol1, sol2):
 
-	if sol1.name == sol2.name:
-		winner = sol1
-		return sol1
+    sol1.health = sol1.baseHealth
+    sol2.health = sol2.baseHealth
 
-	first = random.randint(1, 2)
+    if sol1.id == sol2.id:
+        winner = sol1
+        return sol1
 
-	print('Health of', sol1.name, sol1.health)
-	print('Armor of', sol1.name, sol1.armor)
-	print('Damage of', sol1.name, sol2.damage)
+    first = random.randint(1, 2)
 
-	print('Health of', sol2.name, sol2.health)
-	print('Armor of', sol2.name, sol2.armor)
-	print('Damage of', sol2.name, sol2.damage)
+#    print('Health of', sol1.name, sol1.health)
+#    print('Armor of', sol1.name, sol1.armor)
+#    print('Damage of', sol1.name, sol2.damage)
 
-	rounds = 0
+#    print('Health of', sol2.name, sol2.health)
+#    print('Armor of', sol2.name, sol2.armor)
+#    print('Damage of', sol2.name, sol2.damage)
 
-	if first == 1:
-		while (sol1.health > 0) and (sol2.health > 0) and (rounds < 3000):
-			sol1.attack(sol2)
-			if sol2.health > 0:
-				sol2.attack(sol1)
-			rounds += 1
-		if sol1.health > 0:
-			winner = sol1
-			return winner
-		elif sol2.health > 0:
-			winner = sol2
-			return winner
-		else:
-			return None
+    rounds = 0
 
-	else:
-		while (sol1.health > 0) and (sol2.health > 0) and (rounds < 3000):
-			sol2.attack(sol1)
-			if sol1.health > 0:
-				sol1.attack(sol2)
-			rounds += 1
-		if sol1.health > 0:
-			winner = sol1
-			return winner
-		elif sol2.health > 0:
-			winner = sol2
-			return winner
-		else:
-			return None
+    if first == 1:
+        while (sol1.health > 0) and (sol2.health > 0) and (rounds < 3000):
+            sol1.attack(sol2)
+            if sol2.health > 0:
+                sol2.attack(sol1)
+            rounds += 1
+        if sol1.health > 0:
+            winner = sol1
+            winner.wins += 1
+            return winner
+        elif sol2.health > 0:
+            winner = sol2
+            winner.wins += 1
+            return winner
+        else:
+            return None
+
+    else:
+        while (sol1.health > 0) and (sol2.health > 0) and (rounds < 3000):
+            sol2.attack(sol1)
+            if sol1.health > 0:
+                sol1.attack(sol2)
+            rounds += 1
+        if sol1.health > 0:
+            winner = sol1
+            winner.wins += 1
+            return winner
+        elif sol2.health > 0:
+            winner = sol2
+            winner.wins += 1
+            return winner
+        else:
+            return None
 
 def matchfighters(soldier_list):
-	tuplelist = []
-	for x in range(0, len(soldier_list)):
+    tuplelist = []
+    for x in range(0, len(soldier_list)):
 
-		if x % 2 == 0:
-			pass
+        if x % 2 == 0:
+            pass
 
-		else:
-			match = (soldier_list[x], soldier_list[x-1])
-			if match[0].name == match[1].name:
-				pass
-			else:
-				print('Item', x, soldier_list[x].name, 'matched with Item', x-1, soldier_list[x-1].name)
-				tuplelist.append(match)
+        else:
+            match = (soldier_list[x], soldier_list[x-1])
+            if match[0].id == match[1].id:
+                pass
+            else:
+#                print('Item', x, soldier_list[x].name, 'matched with Item', x-1, soldier_list[x-1].name)
+                tuplelist.append(match)
 
-	return tuplelist
+    return tuplelist
 
+def writeDNAtoFile(filename, dnaList):
+    #dnaList.sort()
+    with open(filename, 'w') as f:
+        for dna in dnaList.keys():
+            f.write(dna + '    ' + str(dnaList[dna]) + '\n')
 
 def main():
-	soldiers = []
-	iterations = 20
+    soldiers = []
+    varieties = []
+    primeDNA = {}
+    iterations = 10
 
-	abel = soldier('0', baseHealth = 10)
+    abel = soldier('0', baseHealth = 10)
 
-	cain = soldier('1', baseHealth = 13)
+    cain = soldier('1', baseHealth = 13)
 
-	soldiers.append(abel)
-	soldiers.append(cain)
+    soldiers.append(abel)
+    soldiers.append(cain)
 
-	for x in range(0, iterations):
-		
-		matches = matchfighters(soldiers)
-		winner_list = []
+    for x in range(0, iterations):
 
-		print('Round', x, 'of', iterations)
-		
-		for match in matches:
+        matches = matchfighters(soldiers)
+        winner_list = []
 
-			print(match[0].name, 'vs.', match[1].name)
+        print('Round', x, 'of', iterations)
 
-			winner = fight(match[0], match[1])
+        for match in matches:
 
-			if winner != None:
-				winner_list.append(winner)
+#            print(match[0].name, 'vs.', match[1].name)
 
-			try:
-				print('Match Complete,', winner.name, 'won with', winner.health, 'health remaining')
-			except:
-				print('Both combatants died')
+            winner = fight(match[0], match[1])
 
-		soldiers = winner_list.copy()
+            if winner != None:
+                winner_list.append(winner)
 
-		for victor in winner_list:
-			victor.health = victor.baseHealth
-			for k in range(0, 3):
-				print(k)
-				soldiers.append(reproduce(victor))
+#            try:
+#                print('Match Complete,', winner.name, 'won with', winner.health, 'health remaining')
+#            except:
+#                print('Both combatants died')
 
-		try:
-			print(soldiers[0].name)
-		except:
-			print('There are no winners for this round')
+        soldiers = winner_list.copy()
+
+        for victor in winner_list:
+            victor.health = victor.baseHealth
+            for k in range(0, 4):
+                soldiers.append(reproduce(victor))
+
+        for fighter in soldiers:
+            if fighter.dna not in varieties:
+                varieties.append(fighter.dna)
+                primeDNA[fighter.dna] = fighter.wins
+            else:
+                primeDNA[fighter.dna] += fighter.wins
+        
+
+    sortedDNA = OrderedDict(sorted(primeDNA.items(), key=lambda primeDNA: primeDNA[1], reverse=True))
+
+    writeDNAtoFile('dna.txt', sortedDNA)
 
 main()
 
